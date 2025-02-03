8fa1ed5e2b1f58b7a2b590803f07a4bf5358bac6
Rehan Khan <Rehansk123.rk@gmail.com>
2025-02-03 19:12:45 +0530

    second commit

--- DOCKER_DJANGO/app/tests.py
+++ DOCKER_DJANGO/app/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from unittest.mock import Mock, patch
+from django.urls import reverse, resolve
+import requests
+from . import views
 
+
+class ViewTest(TestCase):
+    def test_home(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)    
+        self.assertTemplateUsed(response, 'Weather_app.html') 
+
+class URLTest(TestCase):
+    def test_url_home(self):
+        url = reverse("home")
+        self.assertEqual(resolve(url).func, views.home)
+
+    def test_url_weather(self):
+        url = reverse("weather")
+        self.assertEqual(resolve(url).func, views.weather)
+    
 # Create your tests here.
